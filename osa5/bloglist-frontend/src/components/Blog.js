645727e762e38c69201693762fba8ccb7a6c16a1
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2018-10-24 21:02:51 +0300

    5.6

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,44 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      detailsVisible : false,
+      blog: props.blog
+    }
+  }
+
+
+  toggleDetailsVisibility = () => {
+    this.setState({detailsVisible: !this.state.detailsVisible})
+  }
+
+  render() {
+
+    const detailsStyle = { display: this.state.detailsVisible ? '' : 'none', marginLeft: '1em'}
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+    
+    const lisannyt = this.state.blog.user ? "added by " + this.state.blog.user.name : ""
+
+    return (      
+      <div onClick={this.toggleDetailsVisibility} style={blogStyle}>
+        {this.state.blog.title} {this.state.blog.author}
+        <div style={detailsStyle}>          
+          <a href={this.state.blog.url}>{this.state.blog.url}</a><br/>
+          {this.state.blog.likes} likes <button>like</button><br/>
+          {lisannyt}
+        </div>
+      </div>  
+    )
+  }
+}
 
 export default Blog
