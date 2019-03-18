e6db3ebd98637994e25af8acda1ad8cc93992d83
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2019-03-18 22:03:18 +0200

    8.2

--- osa8/backend/library-backend.js
+++ osa8/backend/library-backend.js
@@ -84,16 +84,26 @@ const books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String]!,
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
