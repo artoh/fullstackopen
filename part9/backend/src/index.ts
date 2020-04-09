02ee9ddfe08072377e7032fcac646c05e460ad7f
Arto Hyvättinen <arto.hyvattinen@gmail.com>
2020-04-09 17:36:16 +0300

    9.10

--- part9/backend/src/index.ts
+++ part9/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 const cors = require("cors");
+import diagnoseRouter from "./routes/diagnoses";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,8 @@ app.get("/api/ping", (_req, res) => {
   console.log("Someone pinging");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Patientor server running on port ${PORT}`);
